9fd17b35047f3120c4accb245d796e684801e85c
Johnson Pinto <johnson8582@yahoo.com>
2021-04-30 07:41:39 +0000

    some changes johnson

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,5 +35,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    System.out.println("Hello, Johnson");
+    System.out.println("Hello, pinto ");
+    System.out.println("Hello, mangalore");
   }
 }
